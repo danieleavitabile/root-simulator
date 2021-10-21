7e3bb2c038d8f74a11f41c47530d35c34ccea69f
Teresa <teresa.babini@mail.polimi.it>
2021-10-21 17:33:09 +0200

    Add Stationarity Test code, mod run.sh

--- Codes/run.sh
+++ Codes/run.sh
@@ -13,7 +13,9 @@ mkdir results/$STR/U;
 mkdir results/$STR/V;
 # mkdir results/$STR/code/;
 
-FreeFem++ 1cell_semi_implicit.edp 2>&1 | tee results/$STR/output.txt;
+EXEC=1cell_semi_implicit.edp;
+
+FreeFem++ $EXEC 2>&1 | tee results/$STR/output.txt;
 wait;
 # estensione .vtk per gif paraview
 mv results/U*.vtk results/$STR/U;
@@ -22,6 +24,15 @@ mv results/U*.ps results/$STR/U;
 mv results/V*.vtk results/$STR/V;
 mv results/V*.ps results/$STR/V;
 
+mv results/*.txt results/$STR;
 find . -name "*.ps" -exec rename 's/\.ps$/.eps/' '{}' + # cambia estensione .ps in .eps, per latex
 
 echo "--- Script ended ---";
+
+#to make gif with paraview
+# mkdir results/$STR/U/frame_gif; #salvarvi le animation da paraview
+# mkdir results/$STR/V/frame_gif; #salvarvi le animation da paraview
+# cd results/$STR/U/frame_gif;
+# convert -verbose -dispose background -delay 10 *.png animation.gif
+# cd results/$STR/V/frame_gif;
+# convert -verbose -dispose background -delay 10 *.png animation.gif
