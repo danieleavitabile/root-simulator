c545b1a9664b4646c2874d5f4555b2b372225a10
Teresa Babini <teresa.babini@mail.polimi.it>
2022-01-24 12:16:26 +0100

    Add solver for 2 cells, RR and RRmod, and 4cells

--- Codes/run.sh
+++ Codes/run.sh
@@ -9,14 +9,24 @@ echo "----------------------------------------------------";
 mkdir results;
 STR=$(date '+%Y-%m-%d_%H-%M-%S');
 mkdir results/$STR;
+mkdir results/$STR/code/;
 mkdir results/$STR/U;
 mkdir results/$STR/V;
 
-EXEC=1cell_stationaryTest_2.edp;
+EXEC=2cell_RRmod.edp;
 
-FreeFem++ $EXEC 2>&1 | tee results/$STR/output.txt;
+/u/opt/bin/FreeFem++ $EXEC 2>&1 | tee results/$STR/output.txt;
 wait;
-# estensione .vtk per gif paraview
+# cp *.edp results/$STR/code/;
+cp varf_2cellRR.edp results/$STR/code/;
+cp PRM.edp results/$STR/code/;
+cp 2cell_RRmod.edp results/$STR/code/;
+cp assemble_RR.edp results/$STR/code/;
+cp channelPRM.edp results/$STR/code/;
+
+# cp 2cell_geometric.edp results/$STR/code;
+# cp step_semi_implicit.edp results/$STR/code;
+
 mv results/U*.vtk results/$STR/U;
 mv results/U*.ps results/$STR/U;
 mv results/U*.txt results/$STR/U;
@@ -25,6 +35,11 @@ mv results/V*.vtk results/$STR/V;
 mv results/V*.ps results/$STR/V;
 mv results/V*.txt results/$STR/V;
 
+mv results/channel.ps results/$STR;
+mv results/channel1.vtk results/$STR;
+mv results/channel2.vtk results/$STR;
+
 find . -name "*.ps" -exec rename 's/\.ps$/.eps/' '{}' + # cambia estensione .ps in .eps, per latex
 
 echo "--- Script ended ---";
+
