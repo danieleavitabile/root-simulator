292885c86299743cbb7e8aec0ef3a220b884d278
Teresa Babini <teresa.babini@mail.polimi.it>
2022-03-01 14:52:01 +0100

    Change run.sh

--- Codes/run.sh
+++ Codes/run.sh
@@ -19,14 +19,6 @@ FreeFem++ $EXEC 2>&1 | tee results/$STR/output.txt;
 wait;
 
 cp *.edp results/$STR/code/;
-# cp varf_2cellRR.edp results/$STR/code/;
-# cp PRM.edp results/$STR/code/;
-# cp 2cell_RRmod.edp results/$STR/code/;
-# cp assemble_RR.edp results/$STR/code/;
-# cp channelPRM.edp results/$STR/code/;
-
-# cp 2cell_geometric.edp results/$STR/code;
-# cp step_semi_implicit.edp results/$STR/code;
 
 mv results/U*.vtk results/$STR/U;
 mv results/U*.ps results/$STR/U;
@@ -36,9 +28,8 @@ mv results/V*.vtk results/$STR/V;
 mv results/V*.ps results/$STR/V;
 mv results/V*.txt results/$STR/V;
 
-mv results/channel.ps results/$STR;
-mv results/channel1.vtk results/$STR;
-mv results/channel2.vtk results/$STR;
+mv results/channel*.ps results/$STR;
+mv results/channel*.vtk results/$STR;
 
 find . -name "*.ps" -exec rename 's/\.ps$/.eps/' '{}' +
 
